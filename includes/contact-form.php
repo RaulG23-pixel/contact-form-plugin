8113de889d00a6f6a7c44abce142fb9fba3b5556
Raul Gonzalez <raulx12@gmail.com>
2023-07-03 11:30:06 -0400

    WIP

--- includes/contact-form.php
+++ includes/contact-form.php
@@ -7,7 +7,38 @@ use PHPMailer\PHPMailer\Exception;
 add_action("rest_api_init", "create_rest_endpoint");
 add_shortcode("contact","show_contact_form");
 add_action('init', 'register_scripts');
+add_action('init', 'create_submissions_page');
 add_action('wp_enqueue_scripts', 'enqueue_scripts');
+add_action('add_meta_boxes','create_meta_box');
+
+function create_meta_box(){
+    add_meta_box('custom_contact_form','submission','display_submission','submission');
+}
+
+function display_submission(){
+    $postMetas = get_post_meta(get_the_ID());
+    echo "<ul>";
+    unset($postMetas["_edit_lock"]);
+    foreach($postMetas as $key => $value){
+        echo "<li><strong>" . ucfirst($key) . "</strong>" . ': ' . $value[0] . "</li>";
+    }
+    echo "</ul>";
+}
+
+
+function create_submissions_page(){
+    $args = [
+        'public' => true,
+        'has_archive' => true,
+        'labels' => [
+            'name' => 'Submissions',
+            'singular_name' => 'Submission'
+        ],
+        'supports' => false,
+    ];
+
+    register_post_type('submission',$args);
+}
 
 function show_contact_form(){
     include MY_PLUGIN_PATH . "includes/templates/contact-form.php";
@@ -15,10 +46,7 @@ function show_contact_form(){
 
 function register_scripts(){
     $css = plugins_url('css/contact.css',__FILE__);
-    $js = plugins_url('js/contact-form.js',__FILE__);
-
     wp_register_style('contact-form-css',$css);
-    wp_register_script('contact-form-scripts', $js);
 }
 
 function enqueue_scripts(){
@@ -51,8 +79,16 @@ function handle_enquiry($data){
     $message = '';
     $message = "Message has been sent from {$params['name']} <br></br>";
 
+    $postArgs = [
+            'post_title' => $params["name"],
+            'post_type' => 'Submission'
+        ];
+        
+    $post_id = wp_insert_post($postArgs);
+
     foreach($fields as $label => $value){
         $message .= ucfirst($label) .': ' . $value .'<br>';
+        add_post_meta($post_id,$label,$value);
     }
    
     function mailtrap(PHPMailer $phpmailer) {
@@ -76,12 +112,10 @@ function handle_enquiry($data){
         $mail->isHTML(true);
 
         $phpmailer->send();
-        echo 'Message has been sent';
+        return new WP_REST_Response("Message has been sent", 200);
+
     } catch (\Throwable $e) {
-        return $e->getMessage();
+        return new WP_REST_Response("There was an error sending the email", 500);
     }
-    
-
-
       
 }
--- includes/templates/contact-form.php
+++ includes/templates/contact-form.php
@@ -1,3 +1,9 @@
+<div id="form_success" style="background-color: green; color: white;">
+</div>
+
+<div id="form_failed" style="background-color: red; color: white;">
+</div>
+
 <form action="#" id="enquiry_form" class="enquiry_form">
     <?php wp_nonce_field('wp_rest');?>
 
@@ -18,18 +24,25 @@
 </form>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script>
-    $(document).ready(function(x){
-        alert("funciona");
-    $("#enquiry_form").submit(function(x){
+    jQuery(document).ready(function(x){
+    jQuery("#enquiry_form").submit(function(x){
         x.preventDefault();
 
-        let form = $(this);
+        let form = jQuery(this);
         let url = "<?php echo get_rest_url(null,'v1/contact-form/submit'); ?>";
-        $.ajax({
+        jQuery.ajax({
             type: "POST",
             url: url,
-            data: form.serialize()
+            data: form.serialize(),
+            success: function(res){
+                jQuery("#form_success").html(res).fadeIn();
+                
+            },
+            error: function(){
+                jQuery("#form_error").html("There has been an error on sending the message").fadeIn();
+            }
         });
+
     });
 })
 </script>
